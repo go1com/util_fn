382f83f39a85e3c3df20c1cd2884480d6931925a
Andy Truong <thehongtt@gmail.com>
2018-08-09 16:23:42 +0700

    Remove debug code.

--- Fn.php
+++ Fn.php
@@ -43,14 +43,14 @@ class Fn
                 'type'    => 'error',
                 'code'    => $e->getCode(),
                 'message' => $e->getMessage(),
-                # 'trace'   => $e->getTrace(),
+                'trace'   => $e->getTrace(),
             ];
         } catch (NonretriableException $e) {
             $response = [
                 'type'    => 'error',
                 'code'    => $e->getCode(),
                 'message' => $e->getMessage(),
-                # 'trace'   => $e->getTrace(),
+                'trace'   => $e->getTrace(),
             ];
         } catch (UnauthorizedHttpException $e) {
             $response = [
